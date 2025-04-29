f90b00536c898c671dd8a6c3e14e3b116730fd7e
Gabrie50 <gcriste268@gmail.com>
2025-04-29 17:50:48 -0300

    Add files via upload

---
+++ binder-keepalive/index.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+const BINDER_URL = "https://hub.gesis.mybinder.org/user/gabrie50-jupyter-desktop-server-r227cdea/desktop/";
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  });
+
+  const page = await browser.newPage();
+  await page.goto(BINDER_URL, { waitUntil: 'networkidle2' });
+
+  console.log("Binder link aberto com sucesso. Mantendo ativo...");
+
+  // Mantém o navegador rodando
+  setInterval(async () => {
+    console.log("Pingando Binder...");
+    await page.reload({ waitUntil: 'networkidle2' });
+  }, 5 * 60 * 1000); // a cada 5 minutos
+})();
