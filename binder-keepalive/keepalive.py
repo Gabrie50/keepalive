1f94d2f6d2e502f152edf0d25699407c4448b68b
Gabrie50 <gcriste268@gmail.com>
2025-05-01 11:48:12 -0300

    Update keepalive.py

--- binder-keepalive/keepalive.py
+++ binder-keepalive/keepalive.py
@@ -1,21 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import chromedriver_autoinstaller
 import time
 
 URL = "https://hub.gesis.mybinder.org/user/gabrie50-jupyter-desktop-server-jt7jvpj0/desktop/"
+
+chromedriver_autoinstaller.install()
 
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-# Inicializa o Chrome
 driver = webdriver.Chrome(options=options)
 
 print("Abrindo o Binder...")
 driver.get(URL)
 
-# Mantém a aba ativa com reload a cada 5 minutos
 while True:
     print("Binder ativo... recarregando.")
     time.sleep(300)
